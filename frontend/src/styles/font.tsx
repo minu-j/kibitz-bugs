d79d4ac1c13a54e27f4a199818b256e854b5fe64
ysu6691 <ysu6691@gmail.com>
2023-09-04 15:54:20 +0900

    🐛 font path

--- frontend/src/styles/font.tsx
+++ frontend/src/styles/font.tsx
@@ -1,4 +1,8 @@
 import { Global, css } from "@emotion/react";
+import regular1 from "../assets/fonts/Cafe24Supermagic-Regular-v1.0.woff";
+import regular2 from "../assets/fonts/Cafe24Supermagic-Regular-v1.0.woff2";
+import bold1 from "../assets/fonts/Cafe24Supermagic-Bold-v1.0.woff";
+import bold2 from "../assets/fonts/Cafe24Supermagic-Bold-v1.0.woff2";
 
 function FontStyles() {
   return (
@@ -8,20 +12,18 @@ function FontStyles() {
           font-family: "Cafe24Supermagic";
           font-weight: bold;
           font-style: normal;
-          src: url("src/assets/fonts/Cafe24Supermagic-Bold-v1.0.woff2")
-              format("woff2"),
-            url("src/assets/fonts/Cafe24Supermagic-Bold-v1.0.woff")
-              format("woff");
+          src:
+            url("${bold2}") format("woff2"),
+            url("${bold1}") format("woff");
         }
 
         @font-face {
           font-family: "Cafe24Supermagic";
           font-weight: normal;
           font-style: normal;
-          src: url("src/assets/fonts/Cafe24Supermagic-Regular-v1.0.woff2")
-              format("woff2"),
-            url("src/assets/fonts/Cafe24Supermagic-Regular-v1.0.woff")
-              format("woff");
+          src:
+            url("${regular2}") format("woff2"),
+            url("${regular1}") format("woff");
         }
 
         button:hover {
