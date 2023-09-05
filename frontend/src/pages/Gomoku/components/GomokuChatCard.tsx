dbb1e2f58d481ff58a76052c503e2f054b538ab0
minu-j <minu.j.dev@gmail.com>
2023-09-05 21:29:03 +0900

    🐛 tmi connect error

--- frontend/src/pages/Gomoku/components/GomokuChatCard.tsx
+++ frontend/src/pages/Gomoku/components/GomokuChatCard.tsx
@@ -93,7 +93,7 @@ function GomokuChatCard() {
         username: "gomoku_bot",
         password: user.accessToken ?? "",
       },
-      channels: [user.nickname ?? ""],
+      channels: [user.name ?? ""],
     };
     // Create a client with our options
     const c = new tmi.client(opts);
