650e017c7158e32e0168fc252da001d27739c668
ysu6691 <ysu6691@gmail.com>
2023-09-16 02:43:11 +0900

    ✨ logging

--- backend/src/main/java/com/kibitzbugs/service/GameService.java
+++ backend/src/main/java/com/kibitzbugs/service/GameService.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
