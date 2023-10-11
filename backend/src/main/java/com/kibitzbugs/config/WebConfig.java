b56f926bc10da13a6f458096c79bbae2f5a828fb
ysu6691 <ysu6691@gmail.com>
2023-10-11 23:14:53 +0900

    🚑 cors

--- backend/src/main/java/com/kibitzbugs/config/WebConfig.java
+++ backend/src/main/java/com/kibitzbugs/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .exposedHeaders("ACCESS-TOKEN");
     }
 }
