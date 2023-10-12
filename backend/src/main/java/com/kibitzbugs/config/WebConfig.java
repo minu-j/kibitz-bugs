fe6fe4b45c678a0972271d399b69050c9c0606fd
ysu6691 <ysu6691@gmail.com>
2023-10-12 21:55:05 +0900

    🐛 allow origin

--- backend/src/main/java/com/kibitzbugs/config/WebConfig.java
+++ backend/src/main/java/com/kibitzbugs/config/WebConfig.java
@@ -15,32 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://5eeb-218-50-35-225.ngrok-free.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .exposedHeaders("ACCESS-TOKEN")
                 .exposedHeaders("Set-Cookie");
-    }
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.setAllowCredentials(true);
-        config.addExposedHeader("ACCESS-TOKEN");
-        config.addExposedHeader("Set-Cookie");
-        config.setMaxAge(6000L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter());
-        filterBean.setOrder(0);
-
-        return filterBean;
     }
 }
