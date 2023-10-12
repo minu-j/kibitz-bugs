1ade66a15cdbc103b9cdd380cb017460b7a42068
ysu6691 <ysu6691@gmail.com>
2023-10-12 21:40:01 +0900

    🐛 allow origin

--- backend/src/main/java/com/kibitzbugs/config/WebConfig.java
+++ backend/src/main/java/com/kibitzbugs/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
