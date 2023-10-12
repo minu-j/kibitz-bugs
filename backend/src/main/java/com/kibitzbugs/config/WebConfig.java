2e8636ed252e89f1bc4fa9a4c14bba154c0a4c56
ysu6691 <ysu6691@gmail.com>
2023-10-12 21:48:20 +0900

    🐛 cors config

--- backend/src/main/java/com/kibitzbugs/config/WebConfig.java
+++ backend/src/main/java/com/kibitzbugs/config/WebConfig.java
@@ -1,6 +1,11 @@
 package com.kibitzbugs.config;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +21,26 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .exposedHeaders("ACCESS-TOKEN")
                 .exposedHeaders("Set-Cookie");
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        config.addExposedHeader("ACCESS-TOKEN");
+        config.addExposedHeader("Set-Cookie");
+        config.setMaxAge(6000L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter());
+        filterBean.setOrder(0);
+
+        return filterBean;
     }
 }
